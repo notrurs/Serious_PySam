b04ab6dc8d87f0d0bd7649715b491ea386f7d9fb
notrurs <fursik111@gmail.com>
2020-04-22 15:03:37 +0300

    Now game starts with menu and few args import from config

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 import pygame
 import sys
+from collections import defaultdict
+from pygame_menu.locals import ALIGN_CENTER
 
 import config as c
-from collections import defaultdict
+from menu import MainMenu
 
 
 class Game:
@@ -20,12 +22,10 @@ class Game:
         self.bullets = []
         pygame.mixer.init(44100, 16, 2, 4096)
         pygame.init()
-        pygame.mixer.music.load(c.music_fight)
-        pygame.mixer.music.play(-1)
         self.channel_hero_fire = pygame.mixer.Channel(0)
-        self.channel_hero_fire.set_volume(0.5)
+        self.channel_hero_fire.set_volume(c.hero_fire_volume)
         self.channel_enemy_sound = pygame.mixer.Channel(1)
-        self.channel_enemy_sound.set_volume(0.3)
+        self.channel_enemy_sound.set_volume(c.enemy_sound_volume)
         self.surface = pygame.display.set_mode((width, height))
         pygame.display.set_caption(caption)
         self.clock = pygame.time.Clock()
@@ -58,7 +58,11 @@ class Game:
                 for handler in self.mouse_handlers[event.type]:
                     handler(event.type, event.pos)
 
-    def run(self):
+    def start_game(self):
+        pygame.mixer.music.load(c.music_fight)
+        pygame.mixer.music.set_volume(c.music_volume)
+        pygame.mixer.music.play(-1)
+
         while not self.game_over:
             self.surface.blit(self.background_image, (0, 0))
 
@@ -68,3 +72,26 @@ class Game:
 
             pygame.display.update()
             self.clock.tick(self.frame_rate)
+
+    def create_main_menu(self):
+        pygame.mixer.music.load(c.menu_music)
+        pygame.mixer.music.set_volume(c.menu_music_volume)
+        pygame.mixer.music.play(-1)
+
+        menu_control = MainMenu()
+        menu_control.add_label(c.menu_control_label, align=ALIGN_CENTER, font_size=30,  margin=(0, 100))
+        menu_control.add_button('Назад', menu_control.event_back)
+
+        menu = MainMenu()
+        menu.add_button('Новая игра', self.start_game)
+        menu.add_button('Управление', menu_control)
+        menu.add_button('Выход', menu.event_quit)
+
+        menu.mainloop(self.surface)
+
+    def background_function(self):
+        bg_img = pygame.image.load(c.menu_background_image)
+        self.surface.blit(bg_img, (0, 0))
+
+    def run(self):
+        self.create_main_menu()
