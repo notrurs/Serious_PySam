b9f8097b28f029fa9ea540b583c376ed03b7c9eb
notrurs <fursik111@gmail.com>
2020-04-23 12:17:23 +0300

    Now sound inherited from GameObject, delete unused import

--- bullet.py
+++ bullet.py
@@ -1,14 +1,11 @@
-import pygame
-
 import config as c
 from game_object import GameObject
 
 
 class Bullet(GameObject):
     def __init__(self, x, y, picture, speed, sound):
-        GameObject.__init__(self, x, y, picture, speed)
+        GameObject.__init__(self, x, y, picture, speed, sound)
         self.speed = c.bullet_speed
-        self.sound = pygame.mixer.Sound(sound)
 
     def update(self):
         self.move(self.speed, 0)
