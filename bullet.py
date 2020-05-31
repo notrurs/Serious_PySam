e0b0ec03de9dac3ffca388b0721f83d127e1a1d4
notrurs <fursik111@gmail.com>
2020-05-31 08:27:33 +0300

    Change bullet speed var

--- bullet.py
+++ bullet.py
@@ -5,7 +5,4 @@ from game_object import GameObject
 class Bullet(GameObject):
     def __init__(self, x, y, picture, speed, sound):
         GameObject.__init__(self, x, y, picture, speed, sound)
-        self.speed = c.BULLET_SPEED
-
-    def update(self):
-        self.move(self.speed, 0)
+        self.speed = speed
