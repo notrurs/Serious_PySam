2d312029f84c0b9075d8cdc37a82f57f29e5c49a
notrurs <fursik111@gmail.com>
2020-06-22 08:25:52 +0300

    Add hp and damage from config

--- serious_pysam/game_object/entity/kamikaze.py
+++ serious_pysam/game_object/entity/kamikaze.py
@@ -1,10 +1,14 @@
 from serious_pysam.game_object.game_object import GameObject
+from serious_pysam.config import ENEMY_KAMIKAZE_HP
+from serious_pysam.config import ENEMY_KAMIKAZE_DAMAGE
 
 
 class Kamikaze(GameObject):
     def __init__(self, x, y, picture, speed, sound, channel):
         GameObject.__init__(self, x, y, picture, speed, sound, channel)
         self.speed = speed
+        self.health = ENEMY_KAMIKAZE_HP
+        self.damage = ENEMY_KAMIKAZE_DAMAGE
 
     def update(self):
         self.move(-self.speed, 0)
