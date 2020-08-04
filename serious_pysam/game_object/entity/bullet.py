57090d52b6e5d56741a72770267b62ccd872addf
notrurs <fursik111@gmail.com>
2020-08-04 13:43:29 +0300

    Add comments for bullet class

--- serious_pysam/game_object/entity/bullet.py
+++ serious_pysam/game_object/entity/bullet.py
@@ -8,7 +8,17 @@ from serious_pysam.game_object.game_object import GameObject
 
 
 class Bullet(GameObject):
+    """Describes bullet behaviour.
+
+    The main goal of bullet is movement in a given direction.
+
+    """
     def __init__(self, x, y, picture, speed, sound, damage, objects_positions=None):
+        """Bullet has own vars:
+        damage - bullet damage
+        obj_pos - a tuple, which contains target position and attacker position
+
+        """
         GameObject.__init__(self, x, y, picture, speed, sound)
         self.speed = (speed, 0)
         self.damage = damage
@@ -16,6 +26,14 @@ class Bullet(GameObject):
         self.rotate_bullet(objects_positions)
 
     def rotate_bullet(self, objects_positions):
+        """Rotate bullet picture and sets movement direction in two axises.
+
+        :param objects_positions: contains target position and attacker position,
+        so bullet can know in which direction it need to move. If value is None,
+        bullet moves only X axis
+        :type objects_positions: list, tuple
+
+        """
         if objects_positions is not None:
             target_pos = objects_positions[0]
             attacker_pos = objects_positions[1]
