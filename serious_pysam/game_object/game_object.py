c62773eb821db5536307274e30ae49f418549580
notrurs <fursik111@gmail.com>
2020-08-04 13:44:17 +0300

    Add comments for game object class

--- serious_pysam/game_object/game_object.py
+++ serious_pysam/game_object/game_object.py
@@ -3,7 +3,31 @@ from pygame.mixer import Sound
 
 
 class GameObject:
+    """A super class for hero, enemies, boss and bullets.
+
+    Any subclasses has methods below:
+    blit()
+    move()
+    play_sound()
+    update()
+
+    """
     def __init__(self, x, y, picture, speed=(0, 0), sound=None, channel=None):
+        """
+        :param x: Position on x coordinate
+        :type x: int
+        :param y: Position on y coordinate
+        :type y: int
+        :param picture: Object picture on screen
+        :type picture: string path to picture
+        :param speed: Object move speed on the screen
+        :type speed: tuple
+        :param sound: Object soundtrack (e.g. kamikaze scream)
+        :type sound: pygame.mixer.Sound
+        :param channel: Channel for object sound
+        :type channel: pygame.mixer.Channel
+
+        """
         self.image_object = image.load(picture).convert_alpha()
         self.rect = self.image_object.get_rect()
         self.rect.x = x
@@ -13,6 +37,7 @@ class GameObject:
             self.sound = Sound(sound)
             self.channel = channel
 
+    # There are some properties below which uses one-two times and don't need to store them in memory
     @property
     def x(self):
         return self.rect.x
@@ -50,16 +75,30 @@ class GameObject:
         return self.rect.center
 
     def blit(self, surface):
+        """Blits object on the screen.
+
+        :param surface: Surface on the screen
+        :type surface: pygame.Surface
+
+        """
         surface.blit(self.image_object, (self.rect.x, self.rect.y))
 
     def move(self, dx, dy):
+        """Calls rect method move() with dx and dy params.
+
+        :param dx: x offset value
+        :param dy: y offset value
+
+        """
         self.rect = self.rect.move(dx, dy)
 
     def play_sound(self):
+        """Play object sound"""
         if self.channel is not None:
             self.channel.queue(self.sound)
 
     def update(self):
+        """Calls method move"""
         if self.speed == [0, 0]:
             return
         self.move(*self.speed)
