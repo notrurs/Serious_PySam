e700d6beda6fb520b5bbdd3c72acbdd33ca65a78
notrurs <fursik111@gmail.com>
2020-08-04 13:44:59 +0300

    Add comments for kamikaze class

--- serious_pysam/game_object/entity/kamikaze.py
+++ serious_pysam/game_object/entity/kamikaze.py
@@ -4,12 +4,27 @@ from serious_pysam.config import ENEMY_KAMIKAZE_DAMAGE
 
 
 class Kamikaze(GameObject):
+    """Describes kamikaze behaviour.
+
+    The main goal of kamikaze is run from right to left.
+
+    """
     def __init__(self, x, y, picture, speed, sound, channel):
+        """Kamikaze has own vars:
+        health - kamikaze's health value
+        damage - kamikaze's damage
+
+        """
         GameObject.__init__(self, x, y, picture, speed, sound, channel)
-        self.speed = speed
+        self.speed = speed  # Overrided var because kamikaze can run along only one axis
         self.health = ENEMY_KAMIKAZE_HP
         self.damage = ENEMY_KAMIKAZE_DAMAGE
 
     def update(self):
-        self.move(-self.speed, 0)
+        """Overrided method update() form GameObject.
+
+        Move kamikaze from right to left.
+
+        """
+        self.move(-self.speed, 0)  # Negative is for from right to left running
         self.play_sound()
