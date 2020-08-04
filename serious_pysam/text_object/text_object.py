edb33b79652faf5b4e92687794c29983f652a75c
notrurs <fursik111@gmail.com>
2020-08-04 13:46:14 +0300

    Add comments for text object class

--- serious_pysam/text_object/text_object.py
+++ serious_pysam/text_object/text_object.py
@@ -2,6 +2,7 @@ import pygame
 
 
 class TextObject:
+    """Class for text objects like hp or score labels."""
     def __init__(self,
                  x,
                  y,
@@ -10,6 +11,22 @@ class TextObject:
                  font_name,
                  font_size,
                  centralized=False):
+        """
+        :param x: Coordinate X
+        :type x: int
+        :param y: Coordinate Y
+        :type y: int
+        :param text_func: Function with text
+        :type text_func: function
+        :param color: (R, G, B) tuple with text color
+        :type color: tuple, list
+        :param font_name: path to the font
+        :type font_name: str
+        :param font_size: Font size
+        :type font_size: int
+        :param centralized: Centralized label or no
+        :type centralized: bool
+        """
         self.x = x
         self.y = y
         self.text_func = text_func
@@ -19,6 +36,7 @@ class TextObject:
         self.centralized = centralized
 
     def blit(self, surface):
+        """Blits text label"""
         text_surface, self.bounds = self.get_surface(self.text_func())
 
         if self.centralized:
@@ -29,8 +47,10 @@ class TextObject:
         surface.blit(text_surface, pos)
 
     def get_surface(self, text):
+        """Returns text surface and text rect"""
         text_surface = self.font.render(text, True, self.color)
         return text_surface, text_surface.get_rect()
 
     def update(self):
+        """All game objects must have this method, but labels don't need it, so use this plug"""
         pass
